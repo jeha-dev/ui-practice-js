99f7f2bda46a7bc9d5a101d69564d43b07faf5da
LEE JEHA <leejeha.dev@gmail.com>
2023-12-06 17:24:33 +0900

    수정

--- js/tab.js
+++ js/tab.js
@@ -1,31 +1,28 @@
 'use strict';
 
 // tab
-const tabLink = document.querySelectorAll('.tab-link-underline');
+const tabLinkItem = document.querySelectorAll('.tab-link-underline');
 const tabContItem = document.querySelectorAll('.tab-cont-item');
-for (let i = 0; i < tabLink.length; i++) {
+for (let i = 0; i < tabLinkItem.length; i++) {
     //default
-    tabLink[0].classList.add("active");
-    tabContItem[0].classList.add("active");
-    // 클릭했을때
-  tabLink[i].addEventListener('click', function (e) {
-    // a default 해제
-    e.preventDefault();
 
+    //clicked
+    tabLinkItem[i].addEventListener('click', function (e) {
+        e.preventDefault();
+        
     // 원래 타겟에서 href값 가져옴
     let orgTarget = e.target.getAttribute('href');
     // tab할 타켓은 href에서 #뺀 값
     let tabTarget = orgTarget.replace('#', '');
-
     for (let x = 0; x < tabContItem.length; x++) {
       tabContItem[x].classList.remove("active");
+      document.getElementById(tabTarget).classList.add("active");
     }
-    document.getElementById(tabTarget).classList.add("active");
-
-    //tablink add active
-    for (let y = 0; y < tabLink.length; y++) {
-      tabLink[y].classList.remove('active');
+    //tabLinkItem add active
+    for (let y = 0; y < tabLinkItem.length; y++) {
+        tabLinkItem[y].classList.remove('active');
       e.target.classList.add('active');
     }
+
   });
 }
